09bbcf3eba56bfb41c21b330abcda9beb51fea5d
kojima <kojima@mi.cs.titech.ac.jp>
2013-09-02 21:42:25 +0900

    support probfi-type predicates for prefix

--- src/prolog/prefix/prefix.pl
+++ src/prolog/prefix/prefix.pl
@@ -42,39 +42,52 @@ prprobefi(Goal):-
   prprobefi(Goal,Expls),print_graph(Expls,[lr('<=>')]).
 
 prprobfi(Goal,Expls) :-
-  $prprobfi(Goal,Expls,1).
+  $prprobfi(Goal,_,1,Expls).
 prprobefi(Goal,Expls) :-
-  $prprobfi(Goal,Expls,0).
+  $prprobfi(Goal,_,0,Expls).
 
-find_scc(Goal,Components) :-
+find_scc(Goal,Components,CompT) :-
   % Testing goal
   probefi(Goal,ExpGraph),
   % Transforming graph
   $pp_trans_graph(ExpGraph,HGraph,_,_),
   % Finding SCC
-  $pp_find_scc(HGraph,Components,_).
+  $pp_find_scc(HGraph,Components,CompT).
 
-$prprobfi(Goal,Expls,Decode) :-
+replace_prob(Id,Mapping,P):-
+  !,(X=(Id,P),member(X,Mapping)),!.
+
+$prprobfi(Goal,OrgExpls,Decode,NewExpls) :-
   % Testing goal
-  probefi(Goal,ExpGraph),
+  (Decode=0->probefi(Goal,OrgExpls);probfi(Goal,OrgExpls)),
   % Transforming graph
-  $pp_trans_graph(ExpGraph,HGraph,_,_),
+  $pp_trans_graph(OrgExpls,HGraph,_,_),
   % Finding SCC
   $pp_find_scc(HGraph,Components,CompTable),
   % Solving graph
   $pp_solve_graph(HGraph,Components,CompTable,ProbTable),
   bigarray_length(ProbTable,Size),
-  % Call Gabow algorithm on each node
-  foreach(I in 1..Size,
-    [Index,Index2,Temp],
-    (
-      Index is Size - I+1,
-      Index2 is I-1,
-      bigarray_get(ProbTable,Index,Temp),
-      $pc_set_gnode_inside(Index2,Temp)
-    )
-  ),!,
+  % Creating mapping from ProbTableIndex to NodeID
+  %new_bigarray(Mapping,Size),
+  %foreach((E,I) in (OrgExpls,1..Size),[Id,T1,T2],
+  %  (E=node(Id,T1,T2),bigarray_put(Mapping,I,Id))),
+  % Creating mapping ProbTableIndex and NodeID
+  Src @= [Index : Index in 1..Size],
+  maplist(I,Ex,Pair,(
+      bigarray_get(ProbTable,I,Temp),
+      Ex=node(Id,_,_),
+      %bigarray_get(Mapping,I,Index2),
+      Pair=(Id,Temp)
+    ),Src,OrgExpls,IMapping),!,
   $pc_import_sorted_graph_size(ESize),
-  $pp_build_expls(ESize,Decode,1,Goal,Expls).
+  % Replacing probabilities
+  maplist(E,NewExpl,(E=node(Id,Paths,P),
+  maplist(Path,NewPath,(Path=path(GNodes,SNodes,PP),
+  maplist(GNode,NewGNode,(GNode=gnode(GID,GP),replace_prob(GID,IMapping,NewGP),NewGNode=gnode(GID,NewGP)),GNodes,NewGNodes)
+  ,NewPath=path(NewGNodes,SNodes,PP)),Paths,NewPaths)
+  ,replace_prob(Id,IMapping,NewP),NewExpl = node(Id, NewPaths ,NewP) ),OrgExpls,NewExpls),
+  % TODO:Re-calculate path-probabilities
+  %
+  $pp_garbage_collect.
 
 
