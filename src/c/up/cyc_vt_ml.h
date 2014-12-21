ecdcc955dd9b398fbef5bff19679f09212cde25f
naegawa <naegawa@nab.flnet.org>
2014-12-22 00:49:43 +0900

    support cyc_vlearn_p

---
+++ src/c/up/cyc_vt_ml.h
@@ -0,0 +1,7 @@
+
+#ifndef CYC_VT_ML_H
+#define CYC_VT_ML_H
+
+int run_cyc_vt(VT_ENG_PTR vt_ptr);
+
+#endif /* CYC_VT_ML_H */
---
+++ src/c/up/cyc_vt_preds.h
@@ -0,0 +1,8 @@
+#ifndef VT_PREDS_H
+#define VT_PREDS_H
+
+int pc_prism_cyc_vt_4(void);
+//int pc_prism_cyc_vbvt_2(void);
+//int pc_prism_cyc_both_vt_2(void);
+
+#endif /* VT_PREDS_H */
