dd750082fb96133d62830b7472e31bd89d8bdbca
Yoshitaka Kameya <kameya@cosmos.(none)>
2015-08-16 16:00:15 +0900

    Updated documents (the manual and program examples) for 2.2

--- exs/exs_lin/rule.pl
+++ exs/exs_lin/rule.pl
@@ -1,7 +1,25 @@
-nonterminal(X):-not terminal(X).
+%%%%
+%%%%  Grammar-dependent part to be included in prefix_pcfg.psm --- rule.pl
+%%%%
+%%%%  Copyright (C) 2014-2015
+%%%%    Sato Laboratory, Dept. of Computer Science,
+%%%%    Tokyo Institute of Technology
+%%%%
+
+%% The clauses in this file specify a probabilistic context-free grammar:
+%%
+%%   s --> s s (prob. 0.4)
+%%   s --> a (0.3)
+%%   s --> b (0.3)
+%%
+%% In particular, its probabilistic behavior is specified by a multi-valued
+%% switch declaration (values/3).
+
+start_symbol(s).
+
+nonterminal(X) :- not terminal(X).
 
 terminal(a).
 terminal(b).
 
-%values(s,[[s,s],[a]]).
 values(s,[[s,s],[a],[b]],[0.4,0.3,0.3]).
