39974246075682ffbed67c1031500500d0cc8a13
kojima_git <kojima@cyb.mei.titech.ac.jp>
2015-08-30 00:31:22 +0900

    devide nonlinear_eq.{cpp/h} into nonlinear_eq.{cpp/h} and cyc_em.{cpp/h}

---
+++ src/c/up/cyc_em.h
@@ -0,0 +1,8 @@
+#ifndef __CYC_EM_H__
+#define __CYC_EM_H__
+
+int pc_linear_eq_2(void);
+int pc_cyc_em_7(void);
+
+#endif /* __CYC_EM_H__ */
+
--- src/c/up/nonlinear_eq.h
+++ src/c/up/nonlinear_eq.h
@@ -1,10 +1,9 @@
-#ifndef __INFIX_H__
-#define __INFIX_H__
+#ifndef __NONLINEAR_H__
+#define __NONLINEAR_H__
 
 int pc_nonlinear_eq_2(void);
-int pc_linear_eq_2(void);
 double compute_nonlinear_viterbi(int nl_debug_level);
 int pc_compute_nonlinear_viterbi_6(void);
-int pc_cyc_em_7(void);
 
-#endif /* __INFIX_H__ */
+#endif /* __NONLINEAR_H__ */
+
