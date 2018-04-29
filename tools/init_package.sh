446e0ac32f5f7bc8ca2ac48e22dd4e2c7ab36603
kojima <kojima.ryosuke.8e@kyoto-u.ac.jp>
2018-04-29 16:09:39 +0900

    add external

---
+++ tools/init_package.sh
@@ -0,0 +1,24 @@
+src=../bin
+dest=./prism/bin
+project=./prism
+
+echo "... exporting git project"
+git clone ssh://kameya_lab/root/prism.git ./prism
+
+rm -rf $project/.git
+
+echo "... copying *.bin/*.exe"
+cp $src/prism_up_linux.bin $dest/
+cp $src/prism_mp_linux.bin $dest/
+cp $src/prism_up_darwin.bin $dest/
+cp $src/prism_mp_darwin.bin $dest/
+cp $src/prism_win32.exe $dest/
+cp $src/prism_win64.exe $dest/
+
+echo "... copying *.out "
+cp $src/prism.out $dest/
+cp $src/foc.out $dest/
+cp $src/batch.out $dest/
+cp $src/bp.out $dest/
+cp $src/mpprism.out $dest/
+
