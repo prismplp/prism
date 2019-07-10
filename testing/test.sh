81550b4e81cfee75e8089e497a824327c58a02ae
ryosuke kojima <kojima.ryosuke.8e@kyoto-u.ac.jp>
2019-07-10 16:54:01 +0900

    modify test messages

--- testing/test.sh
+++ testing/test.sh
@@ -14,6 +14,7 @@ for file in `\find ./*.psm -maxdepth 1 -type f`; do
     fi
 done
 echo ""
+echo "================"
 echo "[TEST] irregular_programs/"
 cd ../irregular_programs/
 for file in `\find ./*.psm -maxdepth 1 -type f`; do
@@ -21,7 +22,7 @@ for file in `\find ./*.psm -maxdepth 1 -type f`; do
     upprism $file
     code=$?
     if [ ${code} -ne 0 ]; then
-      echo "[Success]" $file
+      echo "[Success] successfully detected errors:" $file
     else
       exit 1
     fi
